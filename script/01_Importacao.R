9a927828022a355ef2b300dac0129a0c96fadc92
Pedro Chrispim <gitppc@proton.me>
2026-09-18 17:24:14 -0300

    Update 01_Importacao.R

--- script/01_Importacao.R
+++ script/01_Importacao.R
@@ -12,7 +12,7 @@ library(read.dbc)
 # Os arquivos têm o formato BRAAAA.dbc, sendo BR para Brasil, AAAA para o ano de consolidação dos dados.
 
 
-# fucao get data from datasus
+# funcao get data from datasus
 ftp_read_and_bind_rcurl <- function(
     base_url,
     subdir = "",
